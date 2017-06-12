2239abd2b75ba878e82908acb1d895b533405c5c
Cris <cris@dosaygocorporation>
2017-06-12 12:46:52 +0800

    Okay ready for publishing

--- index.js
+++ index.js
@@ -3,31 +3,41 @@
   const typeNameMatcher = /\[object (\w+)]/;
   const $ = Symbol("[[Uint1ArrayInternal]]");
   const INTERNAL_FORMAT = Uint32Array;
-
-  // Node or browser, either is fine
-
-  try { module.exports = Uint1Array; } catch( e ) { Object.assign( self, { Uint1Array } ); }
+  const TYPED_ARRAYS = new Set([
+    "Int8Array",
+    "Uint8Array",
+    "Uint8ClampedArray",
+    "Int16Array",
+    "Uint16Array",
+    "Int32Array",
+    "UInt32Array",
+    "Float32Array",
+    "Float64Array"
+  ]);
 
   // classes 
 
     class Uint1ArrayPrivates {
-      constructor( publics, { length : length = null, view : view = null, 
-                  buffer : buffer = null, byteOffset : byteOffset = 0, 
-                  byteLength : byteLength = null } = {} ) {
+      constructor( publics, { length : length = null, 
+                  buffer : buffer = null, byteOffset : byteOffset = 0 } = {} ) {
 
         let internal;
-        if ( length == null ) length = 0;
+        if ( !! buffer ) {
+          length = buffer.byteLength * 8;
+        } else if ( ! length ) {
+          length = 0;
+        }
 
-        const wordSize = INTERNAL_FORMAT.BYTES_PER_ELEMENT * 8;
+        const wordBytes = INTERNAL_FORMAT.BYTES_PER_ELEMENT;
+        const wordSize = wordBytes * 8;
         const wordSizeMask = wordSize - 1;
         const wordSizeShift = msb_index( wordSize );
         const wordCount = ( length + wordSizeMask ) >> wordSizeShift;
 
-        if ( !! view && !! buffer ) {
-          internal = view;
-          byteOffset = view.byteOffset;
-        } else {
-          buffer = new ArrayBuffer( wordSize * wordCount );
+        if ( !! buffer ) {
+          internal = new INTERNAL_FORMAT( buffer, byteOffset, wordCount );
+        } else  {
+          buffer = new ArrayBuffer( wordBytes * wordCount );
           internal = new INTERNAL_FORMAT( buffer );
         }
 
@@ -48,7 +58,7 @@
         if ( ! this.changed ) {
           return this.array;
         }
-        const array = this.array = new Uint8Array( this.length );
+        const array = this.array = new Array( this.length );
         for( let i = 0; i < this.length; i++ ) {
           array[i] = this.getBit(i);
         }
@@ -83,14 +93,14 @@
       }
     }
 
-    class Uint1ArrayPublics {
+    class Uint1Array {
       // Uint1Array constructor 
       
         constructor( arg , byteOffset, byteLength ) {
           // find the constructor invocation
           const argType = resolveTypeName(arg);
           
-          let length, privates;
+          let length, privates, temp;
 
           switch( argType ) {
             case "Number":
@@ -100,10 +110,9 @@
               break;
             case "ArrayBuffer":
               const buffer = arg;
-              const view = new INTERNAL_FORMAT( buffer, byteOffset, byteLength );
-              length = view.BYTES_PER_ELEMENT * view.length;
-              privates = new Uint1ArrayPrivates( this, { view, length, buffer } );
+              privates = new Uint1ArrayPrivates( this, { buffer } );
               break;
+            case "Array":
             case "Int8Array":
             case "Uint8Array":
             case "Uint8ClampedArray":
@@ -113,28 +122,38 @@
             case "UInt32Array":
             case "Float32Array":
             case "Float64Array":
-              length = arg.length * arg.BYTES_PER_ELEMENT; 
-              view = arg;
-              privates = new Uint1ArrayPrivates( this, { view, length, buffer : view.buffer } );
+            case "Uint1Array":
+              temp = create_from_iterable( arg );
+              privates = new Uint1ArrayPrivates( this, { length : temp.length } );
+              temp.forEach( (val, i) => privates.setBit( i, val ) );
+              break;
+              break;
             case "Undefined":
             case "Null":
             case "RegExp":
             case "Infinity":
-              arg = null;
               length = 0;
               privates = new Uint1ArrayPrivates( this, { length } );
               break;
             case "Object":
             default:
-              return Uint1Array.from( arg );
+              temp = create_from_iterable( arg );
+              privates = new Uint1ArrayPrivates( this, { length : temp.length } );
+              temp.forEach( (val, i) => privates.setBit( i, val ) );
               break;
           }
 
           // for private internal properties
           Object.defineProperty( this, $, { get: () => privates } );
+
+          // proxy for array-like bracket-accessor via index
+
+          const accessorProxy = new BracketAccessorProxy( this );
+          //this.__proto__ = accessorProxy;
+          return accessorProxy;
           return this;
         }
-      
+
       // Static property slots on the constructor
 
         static get BYTES_PER_ELEMENT() {
@@ -153,22 +172,14 @@
       // Static method slots on the constructor 
 
         static from( iterable ) {
-          const temp = [];
-          let length = 0;
-          for( let item of iterable ) {
-            const bit = toBit( item );
-            temp.push( bit );
-            length++;
-          }
-          const internal = new Uint8ClampedArray( length );
-          internal.set( temp );
-          return new this( internal );
+          const temp = create_from_iterable( iterable );
+          return new Uint1Array( temp );
         }
 
         static of( ...items ) {
-          return this.from( items );
+          return Uint1Array.from( items );
         }
-
+      
       // Property slots on the instances
 
         get buffer() {
@@ -257,12 +268,27 @@
           return this[$].toArray().reduceRight( ...args );
         }
 
-        reverse( ...args ) {
-          return new Uint1Array( this[$].toArray().reverse( ...args ) );
+        reverse() {
+          const temp = this[$].toArray().reverse();
+          this.set( temp );
+          return this;
         }
 
-        set( ...args ) {
-          this[$].toArray().set( ...args );
+        set( arr, offset = 0 ) {
+          //TODO : this has to be very solid
+          const typeName = resolveTypeName(arr);
+          // returning without doing nothing if the argument is 
+          // neither an array nor a typedarray seems to be the 
+          // implemented behaviour in the browser for <TypedArray>.set
+          // and we do not differ here
+          if ( typeName !== "Array" && ! TYPED_ARRAYS.has( typeName ) ) {
+            return;
+          }
+          const last = Math.min( arr.length + offset, this.length );
+          arr = arr.map( v => toBit( v ) );
+          for( let i = offset; i < last; i++ ) {
+            this[i] = arr[i-offset];
+          }
         }
 
         slice( ...args ) {
@@ -270,8 +296,9 @@
         }
 
         sort( ...args ) {
+          console.log( this, this[$], $ );
           const sorting = this[$].toArray().sort( ...args );
-          this.fill( sorting );
+          this.set( sorting );
           return this;
         }
 
@@ -296,13 +323,6 @@
         }
     }
 
-    function Uint1Array( arg, byteOffset = 0, length = null ) {
-      const api = new Uint1ArrayPublics( arg, byteOffset, length );
-      const accessorProxy = new BracketAccessorProxy( api );
-      this.__proto__ = accessorProxy;
-      return this;
-    }
-
   // array bracket-accessor proxy 
 
     function BracketAccessorProxy( typed_array_api ) {
@@ -310,6 +330,7 @@
       const array_accessor_handler = {
         get( _, slot, surface ) {
           const i = typeof slot == "string" ? parseInt(slot) : slot;
+          //console.log("Get", slot);
           if ( Number.isInteger( i ) ) {
             return privates.getBit( i );
           } else {
@@ -318,6 +339,7 @@
         },
         set( _, slot, value, surface ) {
           const i = typeof slot == "string" ? parseInt(slot) : slot;
+          //console.log("Set", slot, value );
           if ( Number.isInteger( i ) ) {
             const bit = toBit( value );
             privates.setBit( i, value );
@@ -332,6 +354,17 @@
 
   // helpers
     
+    function create_from_iterable( iterable ) {
+      const temp = [];
+      let length = 0;
+      for( let item of iterable ) {
+        const bit = toBit( item );
+        temp.push( bit );
+        length++;
+      }
+      return temp;
+    }
+
     function format( u1 ) {
       let connector = ', ';
       if ( u1.length > 10 ) { 
@@ -355,4 +388,8 @@
     function resolveTypeName( thing ) {
       return typeNameMatcher.exec( Object.prototype.toString.call( thing ) )[1];
     }
+
+  // Node or browser, either is fine
+
+  try { module.exports = Uint1Array; } catch( e ) { Object.assign( self, { Uint1Array } ); }
 }
--- tests.js
+++ tests.js
@@ -1,14 +1,50 @@
 "use strict";
 {
+  const Uint1Array = require('./index.js');
+  const TESTS = [
+    [ 'sort', '0001111111' ],
+    [ 'reverse', '0111111010' ],
+    [ 'map', '1111111111', x => x + 1 ],
+    [ 'map', '1010000001', x => x ^ 1 ]
+  ];
+
   test();
 
+  function test() {
+    test_display();
+    test_methods();
+    test_bitfield();
+  }
+
   function test_display() {
-    const Uint1Array = require('./index.js');
     const bits = new Uint1Array(128);
     console.log("Bits", bits );  
   }
 
-  function test() {
-    test_display();
+  function test_methods() {
+    const source = [0,1,0,1,1,1,1,1,1,0];
+    let score = 0;
+    TESTS.forEach( ([method, expected, ...args]) => {
+      const subject = new Uint1Array( Array.from( source ) );
+      const result = exec( subject, method, ...args );
+      const score_1 = result == expected ? "PASS" : "FAIL";
+      if ( score_1 == "PASS" ) score += 1;
+      console.log( `Test ${method} expected ${expected}, got ${result}, OK? ${score_1}` );
+    });
+    console.log( `Tests: ${ TESTS.length }, PASSED: ${ score }, FAILED : ${ TESTS.length - score }` );
   }
+
+  function test_bitfield() {
+    const source = new Uint32Array([ 2378462, 324578634, 3458743 ]);
+    const bits = new Uint1Array( source );
+    console.log( source, bits+'');
+    const bitfield = new Uint1Array( source.buffer ); 
+    console.log( source, bitfield+'');
+  }
+
+  function exec( target, method, ...args ) {
+    const result = target[method](...args);
+    return result.join('');
+  }
+
 }
